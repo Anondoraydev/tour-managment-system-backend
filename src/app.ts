35cdc1832c298b1dd8063157cad2be27835b8a2c
Anondoraydev <Anondo554@gmail.com>
2025-08-20 20:28:06 +0600

    cookie add

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
+import cookieParser from "cookie-parser";
 import cors from "cors";
 import express, { Request, Response } from "express";
-import { globalErrorHandler } from "./app/middlewares/globleErrorHandler"; 
-import { router } from "./app/routes";
+import { globalErrorHandler } from "./app/middlewares/globleErrorHandler";
 import notFound from "./app/middlewares/notFound";
+import { router } from "./app/routes";
 
 const app = express();
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(cors());
 
--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -1,13 +1,24 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes";
+import AppError from "../../errorHelpers/AppError";
 import { catchAsync } from "../../utils/catchAsync";
 import { sendResponse } from "../../utils/sendResponse";
 import { AuthService } from "./auth.service";
 
 const credentialsLogin = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    const LoginInfo = await AuthService.credentialsLogin(req.body);
+    const loginInfo = await AuthService.credentialsLogin(req.body);
+
+    res.cookie("accessToken", loginInfo.accessToken, {
+      httpOnly: true,
+      secure: false,
+    });
+
+    res.cookie("refreshToken", loginInfo.refreshToken, {
+      httpOnly: true,
+      secure: false,
+    });
 
     // const user = await UserService.credentialsLogin(req.body)
 
@@ -15,13 +26,16 @@ const credentialsLogin = catchAsync(
       statusCode: httpStatus.OK,
       success: true,
       message: " User logged in successfully",
-      data: LoginInfo,
+      data: loginInfo,
     });
   }
 );
 const getNewAccessToken = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    const refreshToken = req.headers.authorization;
+    const refreshToken = req.cookies.refreshToken;
+    if (!refreshToken) {
+      throw new AppError(httpStatus.BAD_REQUEST, "Refresh token not found");
+    }
     const tokenInfo = await AuthService.getNewAccessToken(
       refreshToken as string
     );
