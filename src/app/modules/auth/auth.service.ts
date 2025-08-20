9354e2d83b6e35f3dd194c7a709d068e48fa5620
Anondoraydev <Anondo554@gmail.com>
2025-08-20 15:03:11 +0600

    Save my auth service work

---
+++ src/app/modules/auth/auth.service.ts
@@ -0,0 +1,57 @@
+import bcryptjs from "bcryptjs";
+import httpsStatus from "http-status-codes";
+import { envVars } from "../../config/env";
+import AppError from "../../errorHelpers/AppError";
+import { IUser } from "../user/user.interfaces";
+import { User } from "../user/user.model";
+
+const credentialsLogin = async (payload: Partial<IUser>) => {
+  const { email, password } = payload;
+
+  const isUserExist = await User.findOne({ email });
+
+  if (!isUserExist) {
+    throw new AppError(httpsStatus.BAD_REQUEST, "Email does not exist");
+  }
+
+  const isPasswordMatch = await bcryptjs.compare(
+    password as string,
+    isUserExist.password as string
+  );
+
+  if (!isPasswordMatch) {
+    throw new AppError(httpsStatus.BAD_REQUEST, "Password does not match");
+  }
+
+  const jwtPayload = {
+    userId: isUserExist._id,
+    email: isUserExist.email,
+    role: isUserExist.role,
+  };
+
+  const accessToken = genarateToken(
+    jwtPayload,
+    envVars.JWT_ACCESS_SECRET,
+    envVars.JWT_ACCESS_EXPIRES
+  );
+
+  const refreshToken = genarateToken(
+    jwtPayload,
+    envVars.JWT_REFRESH_SECRET,
+    envVars.JWT_REFRESH_EXPIRES
+  );
+
+  // delete isUserExist.password;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { password: pass, ...rest } = isUserExist;
+
+  return {
+    accessToken,
+    refreshToken,
+    user: rest,
+  };
+};
+
+export const AuthService = {
+  credentialsLogin,
+};
