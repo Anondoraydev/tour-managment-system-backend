3a214c56dd8f2b73b846023c3ed55e3876298968
Anondoraydev <Anondo554@gmail.com>
2025-08-23 16:00:17 +0600

    passort added

--- src/app/config/passport.ts
+++ src/app/config/passport.ts
@@ -1,11 +1,61 @@
 import passport from "passport";
-import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import {
+  Strategy as GoogleStrategy,
+  Profile,
+  VerifyCallback,
+} from "passport-google-oauth20";
+import { Role } from "../modules/user/user.interfaces";
+import { User } from "../modules/user/user.model";
 import { envVars } from "./env";
 
 passport.use(
-    new GoogleStrategy({ 
-        clientID: envVars.GOOGLE_CLIENT_ID,
-        clientSecret: envVars.GOOGLE_CLIENT_SECRET,
-        callbackURL: envVars.GOOGLE_CALLBACK_URL
-    },async function( )=>{})
-)
+  new GoogleStrategy(
+    {
+      clientID: envVars.GOOGLE_CLIENT_ID,
+      clientSecret: envVars.GOOGLE_CLIENT_SECRET,
+      callbackURL: envVars.GOOGLE_CALLBACK_URL,
+    },
+    async (
+      accessToken: string,
+      refreshToken: string,
+      profile: Profile,
+      done: VerifyCallback
+    ) => {
+      try {
+        const email = profile.emails?.[0].value;
+
+        if (!email) {
+          return done(null, false, { message: "Email not found" });
+        }
+
+        let user = await User.findOne({ email });
+
+        if (!user) {
+          user = await User.create({
+            email,
+            name: profile.displayName,
+            role: Role.USER,
+            picture: profile.photos?.[0].value,
+            isVerified: true,
+            auths: [
+              {
+                provider: "google",
+                providerId: profile.id,
+              },
+            ],
+          });
+        }
+        return done(null, user, { message: "User created successfully" });
+      } catch (error) {
+        console.log("google strategy error", error);
+        return done(error);
+      }
+    }
+  )
+);
+
+// Fonend localhost:5100 -> localhost:5000/api/v1/auth/google -> passport -> google OAuth consent -> gmail login -> success ->callback url localhost:5100/api/v1/auth/google/callback -> db store-> token
+
+//Bridge == google -> user db store -> token
+// Costom -> email ,password , role: USER, name:... -> register -> DB -> 1 user create
+//Google -> req -> google -> success -> successful:jwt token : Role, email -> DB - store ->  token - api access
